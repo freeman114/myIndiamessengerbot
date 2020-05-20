2b39d16ba5981c367e771dbf3f73424745b6c5b4
Wolf Man <wolfman199311@gmail.com>
2020-05-20 12:54:51 +0300

    connecting facebookpage(fix)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express()
 app.set('port', (process.env.PORT) || 5000)
 // SPIN UP SERVER
 app.listen(app.get('port'), function () {
-  console.log('Running on port', app.get('port'))
+    console.log('Running on port', app.get('port'))
 })
 // PARSE THE BODY
 // app.use(bodyParser.json())
@@ -22,15 +22,18 @@ app.listen(app.get('port'), function () {
 
 // index page
 app.get('/', function (req, res) {
-  res.send('hello world i am a chat bot')
+    res.send('hello world i am a chat bot')
 })
 
 // for facebook to verify
 app.get('/webhooks', function (req, res) {
-  if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge'])
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+
 })
 
 // to send messages to facebook
@@ -38,20 +41,20 @@ app.post('/webhooks', function (req, res) {
     var data = req.body;
     console.log("JSON.stringify(data)");
     console.log(JSON.stringify(data));
-//   var entry = FB.getMessageEntry(req.body)
-  // IS THE ENTRY A VALID MESSAGE?
-//   console.log(entry);
-//   if (entry && entry.message) {
-//     if (entry.message.attachments) {
-//       // NOT SMART ENOUGH FOR ATTACHMENTS YET
-//       FB.newMessage(entry.sender.id, "That's interesting!")
-//     } else {
-//       // SEND TO BOT FOR PROCESSING
-//       Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
-//         FB.newMessage(sender, reply)
-//       })
-//     }
-//   }
+    //   var entry = FB.getMessageEntry(req.body)
+    // IS THE ENTRY A VALID MESSAGE?
+    //   console.log(entry);
+    //   if (entry && entry.message) {
+    //     if (entry.message.attachments) {
+    //       // NOT SMART ENOUGH FOR ATTACHMENTS YET
+    //       FB.newMessage(entry.sender.id, "That's interesting!")
+    //     } else {
+    //       // SEND TO BOT FOR PROCESSING
+    //       Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
+    //         FB.newMessage(sender, reply)
+    //       })
+    //     }
+    //   }
 
-//   res.sendStatus(200)
+    //   res.sendStatus(200)
 })
