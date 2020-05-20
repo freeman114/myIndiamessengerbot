4bc3cdf73d1b13a91572c960562610c03efd5685
Wolf Man <wolfman199311@gmail.com>
2020-05-21 02:15:22 +0300

    integrating

--- app.js
+++ app.js
@@ -1,62 +1,219 @@
-'use strict'
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ */
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
+'use strict';
 
-var Config = require('./config')
-// var FB = require('./connectors/facebook')
-// var Bot = require('./bot')
+// Messenger API integration example
+// We assume you have:
+// * a Wit.ai bot setup (https://wit.ai/docs/quickstart)
+// * a Messenger Platform setup (https://developers.facebook.com/docs/messenger-platform/quickstart)
+// You need to `npm install` the following dependencies: body-parser, express, node-fetch.
+//
+// 1. npm install body-parser express node-fetch
+// 2. Download and install ngrok from https://ngrok.com/download
+// 3. ./ngrok http 8445
+// 4. WIT_TOKEN=your_access_token FB_APP_SECRET=your_app_secret FB_PAGE_TOKEN=your_page_token node examples/messenger.js
+// 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/webhook` as callback URL.
+// 6. Talk to your bot on Messenger!
 
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const express = require('express');
+const fetch = require('node-fetch');
 
-// LETS MAKE A SERVER!
-var app = express()
-app.set('port', (process.env.PORT) || 5000)
-// SPIN UP SERVER
-app.listen(app.get('port'), function () {
-    console.log('Running on port', app.get('port'))
-})
-// PARSE THE BODY
-app.use(bodyParser.json())
+let Wit = null;
+let log = null;
+try {
+    // if running from repo
+    Wit = require('../').Wit;
+    log = require('../').log;
+} catch (e) {
+    Wit = require('node-wit').Wit;
+    log = require('node-wit').log;
+}
 
+// Webserver parameter
+const PORT = process.env.PORT || 5000;
 
-// index page
-app.get('/', function (req, res) {
-    res.send('hello world i am a chat bot')
-})
+// Wit.ai parameters
+const WIT_TOKEN = 'R5GS3OKZAL3FYZZCLRZFOKS3HHF5H7HL';
 
-// for facebook to verify
-app.get('/webhooks', function (req, res) {
-    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
-        console.log(req.query);
-        res.send(req.query['hub.challenge'])
-    } else {
-        console.log(req.query);
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.sendStatus(403);
+// Messenger API parameters
+const FB_PAGE_TOKEN = 'EAADhs54CZBV4BAN0507vBnmAVaNpdCuh3DCQkNINSi9MSCfnKkef1P3a1XLMkTFJLZAgvR3tBCExBQNXYC8lZA7GdYmnr2BbqLhq75g5IIEsHRxW5wpqQxI8GT5IZA1BJooqjYGWVWvyjcwk030c6CZAnk5xlrl5CMZCRZCAasCkeJZAGqKtyZC6dZAe5pVVXOuOgZD';
+if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
+const FB_APP_SECRET = 'eefa395df9bfb2939b74b19f6168231b';
+if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
+
+let FB_VERIFY_TOKEN = null;
+crypto.randomBytes(8, (err, buff) => {
+    if (err) throw err;
+    FB_VERIFY_TOKEN = buff.toString('hex');
+    console.log(`/webhook will accept the Verify Token "${FB_VERIFY_TOKEN}"`);
+});
+
+// ----------------------------------------------------------------------------
+// Messenger API specific code
+
+// See the Send API reference
+// https://developers.facebook.com/docs/messenger-platform/send-api-reference
+
+const fbMessage = (id, text) => {
+    const body = JSON.stringify({
+        recipient: { id },
+        message: { text },
+    });
+    const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+    return fetch('https://graph.facebook.com/me/messages?' + qs, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body,
+    })
+        .then(rsp => rsp.json())
+        .then(json => {
+            if (json.error && json.error.message) {
+                throw new Error(json.error.message);
+            }
+            return json;
+        });
+};
+
+// ----------------------------------------------------------------------------
+// Wit.ai bot specific code
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+    let sessionId;
+    // Let's see if we already have a session for the user fbid
+    Object.keys(sessions).forEach(k => {
+        if (sessions[k].fbid === fbid) {
+            // Yep, got it!
+            sessionId = k;
+        }
+    });
+    if (!sessionId) {
+        // No session found for user fbid, let's create a new one
+        sessionId = new Date().toISOString();
+        sessions[sessionId] = { fbid: fbid, context: {} };
     }
+    return sessionId;
+};
 
-})
+// Setting up our bot
+const wit = new Wit({
+    accessToken: WIT_TOKEN,
+    logger: new log.Logger(log.INFO)
+});
 
-// to send messages to facebook
-app.post('/webhooks', function (req, res) {
-    var data = req.body;
-    console.log("JSON.stringify(data)");
-    console.log(JSON.stringify(data));
-    //   var entry = FB.getMessageEntry(req.body)
-    // IS THE ENTRY A VALID MESSAGE?
-    //   console.log(entry);
-    //   if (entry && entry.message) {
-    //     if (entry.message.attachments) {
-    //       // NOT SMART ENOUGH FOR ATTACHMENTS YET
-    //       FB.newMessage(entry.sender.id, "That's interesting!")
-    //     } else {
-    //       // SEND TO BOT FOR PROCESSING
-    //       Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
-    //         FB.newMessage(sender, reply)
-    //       })
-    //     }
-    //   }
+// Starting our webserver and putting it all together
+const app = express();
+app.use(({ method, url }, rsp, next) => {
+    rsp.on('finish', () => {
+        console.log(`${rsp.statusCode} ${method} ${url}`);
+    });
+    next();
+});
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
-    //   res.sendStatus(200)
-})
+// Webhook setup
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send("This is my facebookMessengerBot.");
+});
+
+// Message handler
+app.post('/webhook', (req, res) => {
+    // Parse the Messenger payload
+    // See the Webhook reference
+    // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+    const data = req.body;
+
+    if (data.object === 'page') {
+        data.entry.forEach(entry => {
+            entry.messaging.forEach(event => {
+                if (event.message && !event.message.is_echo) {
+                    // Yay! We got a new message!
+                    // We retrieve the Facebook user ID of the sender
+                    const sender = event.sender.id;
+
+                    // We could retrieve the user's current session, or create one if it doesn't exist
+                    // This is useful if we want our bot to figure out the conversation history
+                    // const sessionId = findOrCreateSession(sender);
+
+                    // We retrieve the message content
+                    const { text, attachments } = event.message;
+
+                    if (attachments) {
+                        // We received an attachment
+                        // Let's reply with an automatic message
+                        fbMessage(sender, 'Sorry I can only process text messages for now.')
+                            .catch(console.error);
+                    } else if (text) {
+                        // We received a text message
+                        // Let's run /message on the text to extract some entities, intents and traits
+                        wit.message(text).then(({ entities, intents, traits }) => {
+                            // You can customize your response using these
+                            console.log(intents);
+                            console.log(entities);
+                            console.log(traits);
+                            // For now, let's reply with another automatic message
+                            fbMessage(sender, `We've received your message: ${text}.`);
+                        })
+                            .catch((err) => {
+                                console.error('Oops! Got an error from Wit: ', err.stack || err);
+                            })
+                    }
+                } else {
+                    console.log('received event', JSON.stringify(event));
+                }
+            });
+        });
+    }
+    res.sendStatus(200);
+});
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+    console.log(signature);
+
+    if (!signature) {
+        // For testing, let's log an error. In production, you should throw an
+        // error.
+        console.error("Couldn't validate the signature.");
+    } else {
+        var elements = signature.split('=');
+        var method = elements[0];
+        var signatureHash = elements[1];
+
+        var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
+}
+
+app.listen(PORT);
+console.log('Listening on :' + PORT + '...');
