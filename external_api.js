1e0476e7809caa760550bf3e31cef37473fc1f92
Wolf Man <wolfman199311@gmail.com>
2020-06-01 21:48:22 +0300

    shoplist

--- external_api.js
+++ external_api.js
@@ -21,18 +21,18 @@ module.exports = {
             // console.log(result);
             console.log(result.length);
             var shopArray = [];
-            result.forEach(item => {
+            for (i = 0; i<10; i++){
                 // console.log(JSON.stringify(item));
-                if (item.photos){
+                if (item[i].photos){
                     // console.log(item.photos[0].photo_reference);
-                    imageUrl = 'https://maps.googleapis.com/maps/api/place/photo?photoreference=' + item.photos[0].photo_reference + '&key=' + apikey + '&maxwidth=200'
+                    imageUrl = 'https://maps.googleapis.com/maps/api/place/photo?photoreference=' + item[i].photos[0].photo_reference + '&key=' + apikey + '&maxwidth=200'
                 } else {
                     // console.log(request.url);
                     imageUrl = ' https://98faba2c.ngrok.io/webhook/public/images/unsupportedimage.png';
                 }
                 var option = 
                 {
-                    "title": item.name, "image_url": imageUrl,
+                    "title": item[i].name, "image_url": imageUrl,
                     "default_action":
                     {
                         "type": "web_url",
@@ -45,9 +45,34 @@ module.exports = {
                 if ( shopArray.length == 10){
                     callback(shopArray);
                 }
+            }
+            // result.forEach(item => {
+            //     // console.log(JSON.stringify(item));
+            //     if (item.photos){
+            //         // console.log(item.photos[0].photo_reference);
+            //         imageUrl = 'https://maps.googleapis.com/maps/api/place/photo?photoreference=' + item.photos[0].photo_reference + '&key=' + apikey + '&maxwidth=200'
+            //     } else {
+            //         // console.log(request.url);
+            //         imageUrl = ' https://98faba2c.ngrok.io/webhook/public/images/unsupportedimage.png';
+            //     }
+            //     var option = 
+            //     {
+            //         "title": item.name, "image_url": imageUrl,
+            //         "default_action":
+            //         {
+            //             "type": "web_url",
+            //             "url": "https://petersfancybrownhats.com/view?item=103", "webview_height_ratio": "tall"
+            //         },
+            //         "buttons": '[{ "type": "postback", "title": "Booking schedule time", "payload": "schedule_time" }, { "type": "postback", "title": "next shop", "payload": "next_shop" }, { "type": "postback", "title": "previous shop", "payload": "previous_shop" }]'
+            //     };
+            //     shopArray.push(option);
+            //     // console.log(JSON.stringify(shopArray));
+            //     if ( shopArray.length == 10){
+            //         callback(shopArray);
+            //     }
 
 
-            });
+            // });
           });
 
     },
