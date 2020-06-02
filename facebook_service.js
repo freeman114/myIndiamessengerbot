19d9131334c078c2ffe3d19e314a6842d1474c3c
Wolf Man <wolfman199311@gmail.com>
2020-06-03 02:14:11 +0300

    shoplist

--- facebook_service.js
+++ facebook_service.js
@@ -83,13 +83,13 @@ module.exports = {
             console.log(item);
             button = {
                 "type": "postback",
-                "title": JSON.stringify(item),
+                "title": item,
                 "payload": "DEVELOPER_DEFINED_PAYLOAD"
             };
-            console.log(JSON.stringify(buttons));
+            buttons.push(button);
         });
-        buttons.push(button);
         if (array.length == buttons.length) {
+            console.log(JSON.stringify(buttons));
             var options = {
                 'method': 'POST',
                 'url': 'https://graph.facebook.com/v7.0/me/messages?access_token=EAADhs54CZBV4BABhvflRJh3J03zD8zkZBRUtgAFEjm6gruGRyoyX8JZB2bRk8PvzTRTSZBKTZC232llCZBhipVIPPbZCoHgbSZCUgcwqxc1tdvbtOO930vEmCMEHM5JdGnoK7vGBkZBwRijZAAXd43jhG1MFJ4Sko2Sv7Elt9ZAN30SeMHcKsCvXY8M',
