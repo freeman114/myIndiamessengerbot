8b5ff4c8f5619e8d378c1f02dabf47f3f6915fe0
Wolf Man <wolfman199311@gmail.com>
2020-06-09 00:39:36 +0300

    fix

--- public/javascript/timeslot.js
+++ public/javascript/timeslot.js
@@ -1,3 +1,15 @@
 function set_timeslot(){
     console.log('what is timeslot');
+    confirm();
 }
+
+function alert() { 
+    var doc; 
+    var result = confirm("Press a button!"); 
+    if (result == true) { 
+        doc = "OK was pressed."; 
+    } else { 
+        doc = "Cancel was pressed."; 
+    } 
+    document.getElementById("g").innerHTML = doc; 
+} 
