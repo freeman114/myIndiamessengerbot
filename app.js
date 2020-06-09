902a10076995850089d14a734c1ea34e297b0871
Wolf Man <wolfman199311@gmail.com>
2020-06-09 11:42:10 +0300

    fix

--- app.js
+++ app.js
@@ -205,8 +205,9 @@ app.get('/webview', (req, res) => {
     }
 });
 app.post('/webview', (req, res) => {
-    console.log(req);
-    console.log(req.body.text);
+    console.log(JSON.stringify(req));
+    
+    console.log(req.body);
     // httpsMsgs.sendJSON(req, res, {
     //     from: res.result.output.text[0],
     // });
