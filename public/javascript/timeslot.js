d4cd7964c8b9d9c64392f9164a34cee1272cc9c3
Wolf Man <wolfman199311@gmail.com>
2020-06-09 09:59:33 +0300

    fix

--- public/javascript/timeslot.js
+++ public/javascript/timeslot.js
@@ -1,9 +1,19 @@
+
+var m_timeslot = "";
 function set_timeslot(id){
     console.log('what is timeslot');
     console.log(id);
+    m_timeslot = id;
 }
 
 function save_timeslot(){
+    if (!m_timeslot){
+        alert ('choose time in list.');
+    }
+    else {
+        console.log(m_timeslot);
+        
+    }
     console.log("save");
 }
 
