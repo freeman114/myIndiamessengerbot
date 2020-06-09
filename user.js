91cde45184c7a1ca8c29be2f018e1929a2acb0f5
Wolf Man <wolfman199311@gmail.com>
2020-06-09 21:40:24 +0300

    fix

--- user.js
+++ user.js
@@ -143,7 +143,6 @@ module.exports = {
                 console.log(ids);
                 var idss = ids.split("_");
                 var dbo = db.db;
-                console.log(place_id);
                 console.log(slot);
                 var query = { place_id: idss[1] };
                 dbo.collection("shopList_collection").find(query).toArray(async function (err, result) {
