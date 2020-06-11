ae8813ee93bc1f6c9e00e973ecca6be055fc552b
Wolf Man <wolfman199311@gmail.com>
2020-06-11 23:56:33 +0300

    qrcode

--- app.js
+++ app.js
@@ -373,8 +373,8 @@ function receivedPostback(event) {
 function receivedMessage(event) {
     console.log('_____________We received message___________');
     console.log(JSON.stringify(event));
-    setSessionAndUser(senderID);
     var senderID = event.sender.id;
+    setSessionAndUser(senderID);
     var recipientID = event.recipient.id;
     var timeOfMessage = event.timestamp;
     var message = event.message;
@@ -401,7 +401,7 @@ function receivedMessage(event) {
 function sendWelcomeMessage(userId) {
     console.log("______________We received welcomemessage._________________");
     let responseText = "Welcome to Localize. Here you can book your slots for shopping at your nearest shop, Requires delivery of goods or Become a volunteer. What would you like to choose? ";
-    setSessionAndUser(senderID);
+    setSessionAndUser(userId);
     let replies = [
         {
             "content_type": "text",
