772b14779c564079afe2aa184b36d3fa3c602019
Wolf Man <wolfman199311@gmail.com>
2020-06-16 13:27:07 +0300

    getting distance

--- External_API/external_api.js
+++ External_API/external_api.js
@@ -169,9 +169,11 @@ module.exports = {
         };
         request(options, function (error, response) {
             if (error) throw new Error(error);
-            console.log(JSON.stringify(response.body["rows"]));
+            console.log(JSON.stringify(response.body));
             // console.log(JSON.parse(JSON.stringify(response.body)));
-            // var body = JSON.parse(JSON.stringify(response.body));
+            var body = JSON.parse(JSON.stringify(response.body));
+            var arr = body.split("/n");
+            console.log(arr);
             // console.log(JSON.stringify(body).json());
 
             // console.log(JSON.parse(body).rows[0]);
